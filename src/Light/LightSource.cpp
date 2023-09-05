f5b2fae5c3c65a1486ceac90e26afeb63fbb860b
fahlerile <50793160+fahlerile@users.noreply.github.com>
2023-09-05 13:11:11 +0500

    Refactor `vec` structs (#2)
    * Refactor `vec2`
    * Refactor `vec3`
    * Refactor `vec4`

--- src/Light/LightSource.cpp
+++ src/Light/LightSource.cpp
@@ -10,17 +10,17 @@ double LightSource::_calculate_intensity(vec3d point, vec3d L, vec3d normal,
 {
     double intensity_at_point;
 
-    double diffuse_term = normal.dot_product(L) / (normal.magnitude() * L.magnitude());
+    double diffuse_term = (normal * L) / (normal.magnitude() * L.magnitude());
     clamp_at_zero(diffuse_term);
 
     double specular_term;
     if (!std::isnan(p_object->get_shininess()))  // if this object is somewhat shiny
     {
         // "real" reflection vector (assuming the surface is perfectly polished, like a mirror)
-        vec3d R = normal * 2 * normal.dot_product(L) - L;
+        vec3d R = normal * 2 * (normal * L) - L;
         vec3d V = camera_pos - point;  // point -> camera
 
-        specular_term = R.dot_product(V) / (R.magnitude() * V.magnitude());
+        specular_term = (R * V) / (R.magnitude() * V.magnitude());
         clamp_at_zero(specular_term);
         specular_term = std::pow(specular_term, p_object->get_shininess());
     }
