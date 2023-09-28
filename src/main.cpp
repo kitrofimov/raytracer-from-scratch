0490b66f42519f841486ac98638cff42e6dac17b
kirill <kitrofimov39@gmail.com>
2023-09-28 16:35:53 +0500

    Remove unused time counters in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -23,25 +23,11 @@ int main()
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
     std::cout << "Rendered in " << ms.count() <<  "ms" << std::endl;
 
-    unsigned int this_frame_time = 0;
-    unsigned int delta_time = 0;
-    unsigned int prev_frame_time = 0;
-
     // Main loop
     while (window.is_running())
     {
-        this_frame_time = SDL_GetTicks();
-        delta_time = this_frame_time - prev_frame_time;
-
         window.poll_events();
-
-        // window.clear(color_t(0, 0, 0, 255));
-        // Render calls
-
-        // std::cout << "Frametime: " << delta_time << std::endl;
         window.swap_buffers();
-
-        prev_frame_time = this_frame_time;
     }
 
     return 0;
